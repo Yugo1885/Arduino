7cfdecb50a9a957d4cf81a2c856f183ff366c6d4
Yugo <84918859+Yugo1885@users.noreply.github.com>
2021-11-26 20:30:24 +0800

    Update heart.ino

--- LED  matrix/heart.ino
+++ LED  matrix/heart.ino
@@ -1,18 +1,20 @@
-#include <LedControl.h>
-#define ldrPin A0
-int DIN = 12;
-int CS = 11;
-int CLK = 10;
-int ldrValue = 0;
+#include<LedControl.h>
+int DIN = 11;
+int CS = 7;
+int CLK = 13;
+LedControl lc = LedControl(DIN, CLK, CS, 0);
+byte heart[8]={0x66,0xFF,0xDB,0xFF,0xDB,0x66,0x3C,0x18};
 
-LedControl lc = LedControl(DIN,CS,CLK,0);
 
-void setup(){
-  lc.shutdown(0,false);
-  lc.setIntensity(0,15);
-  lc.clearDisplay(0);
-  ldrValue = 0.5*ldrValue + 0.5*analogRead(ldrPin); //與前次的讀取數值相加取平均，為了減少誤差
+void setup() {
+  lc.shutdown(0,false); 
+  lc.setIntensity(0,0);
 }
-  
-void loop(){
-  
+
+void loop() {
+  for(int i=0;i<8;i++) lc.setRow(0,i,heart[i]);
+  lc.setIntensity(0,5);
+  delay(800);
+  lc.setIntensity(0,0);
+  delay(500);  
+}
